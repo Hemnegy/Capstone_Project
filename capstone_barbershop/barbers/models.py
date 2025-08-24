c8a39a3f7a307aa5e819e80b2dbe48b1227854ab
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-08-24 17:42:35 +0000

    added barbers model

--- capstone_barbershop/barbers/models.py
+++ capstone_barbershop/barbers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class BarberProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='barber_profile')
+    specialty = models.CharField(max_length=100)
+    experience_years = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.user.username
+
+class Availability(models.Model):
+    barber = models.ForeignKey(BarberProfile, on_delete=models.CASCADE, related_name='availabilities')
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.barber.user.username} - {self.date}"
