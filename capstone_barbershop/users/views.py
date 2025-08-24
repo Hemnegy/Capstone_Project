46d8be952b5fc4f6ef59b3393c0212784d391a1d
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-08-24 17:48:22 +0000

    added register view

--- capstone_barbershop/users/views.py
+++ capstone_barbershop/users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+# Registration view
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+# Custom JWT token view (optional, to include extra info)
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        token['role'] = user.role
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
