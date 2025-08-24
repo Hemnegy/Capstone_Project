dfec9ae2efe1670f4550334841063da157796cad
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-08-24 16:52:14 +0000

    added apps to settings

--- capstone_barbershop/capstone_barbershop/settings.py
+++ capstone_barbershop/capstone_barbershop/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'users',
+    'barbers',
+    'services',
+    'appointments',
 ]
+
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
