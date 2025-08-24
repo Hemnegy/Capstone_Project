313a584608571bdd9f6664e014d7bb16908b7cdc
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-08-24 17:42:03 +0000

    added users model

--- capstone_barbershop/users/models.py
+++ capstone_barbershop/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('customer', 'Customer'),
+        ('barber', 'Barber'),
+    )
+    phone = models.CharField(max_length=20, blank=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
