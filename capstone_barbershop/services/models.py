2449334a49206ba04cc0042b500685daac817809
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-08-24 17:42:59 +0000

    added services model

--- capstone_barbershop/services/models.py
+++ capstone_barbershop/services/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    duration_minutes = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
