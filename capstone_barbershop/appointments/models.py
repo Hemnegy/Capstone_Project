45e2a43aad607302457b0e3ecd83328cbaf21d67
Kwabena Aboagye-Heming <hemingkwabena@gmail.com>
2025-08-24 17:43:34 +0000

    added appointments model

--- capstone_barbershop/appointments/models.py
+++ capstone_barbershop/appointments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from users.models import User
+from barbers.models import BarberProfile
+from services.models import Service
 
-# Create your models here.
+class Appointment(models.Model):
+    STATUS_CHOICES = (
+        ('booked', 'Booked'),
+        ('cancelled', 'Cancelled'),
+        ('completed', 'Completed')
+    )
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointments')
+    barber = models.ForeignKey(BarberProfile, on_delete=models.CASCADE, related_name='appointments')
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='booked')
+
+    def __str__(self):
+        return f"{self.customer.username} - {self.barber.user.username} - {self.date}"
